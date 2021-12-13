faabe934bba40da49a2c190ebc6a0a86a7bbc14f
AS <yvg68@yandex.ru>
2021-12-13 16:18:49 +0300

    init my app with four fragments

---
+++ app/src/main/java/yvg68/measurer/crc10_20211203/MainActivity.kt
@@ -0,0 +1,18 @@
+package yvg68.measurer.crc10_20211203
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val startFragment = StartFragment()
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container, startFragment)
+            .commit()
+    }
+
+}
